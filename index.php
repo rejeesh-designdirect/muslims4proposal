16d75c9869c8c26f53415f30a66fc964d303d87a
rejeesh-designdirect <rejeesh_designdirect@yahoo.com>
2014-08-22 12:07:04 +0530

    asd
    asf

---
+++ index.php
@@ -0,0 +1,213 @@
+<?php
+ob_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Musilm-Praposel</title>
+<link href="css/layout.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="layout">
+
+	<div class="center-layout">
+    
+    	<?php include('include/header.php'); ?>
+        
+        <div class="white-strip float-l">
+        
+        	<div class="white-caption float-l"> UK’s first muslim matrimony website with an online search feature and offline marriage agency.</div>
+            
+           <?php include('include/menu.php'); ?>
+        
+        </div>
+        
+        <div class="banner float-l">
+        
+      <div class="bann-trans">
+      
+      	<h1>Meet Your Soulmate Today!</h1>
+      <h2>Trusted site used by Muslims Across the UK.
+Premium 1-2-1 and Online Service. 
+Designed Specifically for Muslims.
+Access to chat, email and instant messaging 
+features.
+
+</h2>
+      
+      
+      </div>
+        
+        <img src="images/banner.jpg" width="1200" height="397" alt="Musilm - Praposel" />
+        
+        </div>
+        
+        
+        
+        <?php include('include/search-box.php'); ?>
+        
+        
+        
+        <div class="middle-container float-l">
+        
+        <div class="container">
+        
+        	<div class="container-left float-l">
+            
+            	<div class=" head float-l"><h1>3 Easy Steps to Find Your Soulmate</h1></div>
+                
+                <div class=" threeboxes float-l">
+                
+                	<div class="box float-l">
+                    
+                    	<div class="icon float-l"><img src="images/step1.jpg" width="170" height="120" alt="Musilm Praposel" /></div>
+                        
+                        <div class=" content-icon float-l"> <h3>Register</h3>
+                                    <ul>
+                                    
+                                    
+                                            <li> Create profile for free</li>
+                                            <li>Unlimited search for free</li>
+                                            <li>Upload photos for free</li>
+                                            <li>Fill in personality questionnaire
+                                            to increase chances to find your
+                                            match</li>
+                                    
+                                    </ul>
+                        </div>
+                    
+                    </div>
+                    <div class="box box-padd float-l">
+                    
+                    	<div class="icon float-l"><img src="images/step1.jpg" width="170" height="120" alt="Musilm Praposel" /></div>
+                        
+                        <div class=" content-icon float-l"> 
+                        <h3> Search</h3>
+                                    <ul>
+                                    
+                                    
+                                            <li> View other profiles online</li>
+                                            <li>Use M4P agency support to find partners (Ofline Search)</li>
+                                            <li>Start the proposal process</li>
+                                    
+                                    
+                                    </ul>
+                        </div>
+                    
+                    </div>
+                    <div class="box box-padd float-l">
+                    
+                    	<div class="icon float-l"><img src="images/step1.jpg" width="170" height="120" alt="Musilm Praposel" /></div>
+                        
+                        <div class=" content-icon float-l"> 
+                        <h3> Contact</h3>
+                                    <ul>
+                                    
+                                    
+                                            <li> Contact a potential through the online option</li>
+                                            <li>Use the M4P agency to make initial contact with people on behalf of yourself</li>
+                                            <li>Find a Match</li>
+                                    
+                                    
+                                    </ul>
+                        </div>
+                    
+                    </div>
+                
+                </div>
+                
+                
+                <div class=" membership float-l">
+                
+                
+                	<div class="head2 float-l"><h4>Membership Packages</h4></div>
+                    <div class="content float-l">
+                    
+                    	<div class="member-box float-l">
+                        
+                        	<div class="icon float-l"><img src="images/bronze.jpg" width="250" height="133" alt="Musilm Praposel" /></div>
+                            <div class="content3 float-l">Bronze offers a free online profile. You can search for other profiles.<br /><br /><a href="packages.html">More>></a> </div>
+                        
+                        </div>
+                        
+                        <div class="member-box mem-margin float-l">
+                        
+                        	<div class="icon float-l"><img src="images/silver.jpg" width="250" height="133" alt="Musilm Praposel" /></div>
+                            <div class="content3 float-l">
+                            
+                            	Silver offers an online profile. Members can send and receive messages. <br /><br /><a href="packages.html">More>></a>
+                            
+                            </div>
+                        
+                        </div>
+                        
+                        <div class="member-box mem-margin float-l">
+                        
+                        	<div class="icon float-l"><img src="images/gold.jpg" width="250" height="133" alt="Musilm Praposel" /></div>
+                            <div class="content3 float-l">Gold offers all the services. Searching profiles, messaging and the 1-2-1 service using the match making consultants.<br /> <a href="packages.html">More>></a></div>
+                        
+                        </div>
+                    
+                    </div>
+                
+                </div>
+                
+                <div class="content2 float-l">
+                
+                <h3>Why Choose Muslims4Propsal?</h3><br />
+
+<p>Muslims4proposal.com has helped thousands of Muslim singles find their match. As one of the leading Islamic matrimonial sites, we are one of the largest and most trusted sites around. Not many other sites can offer you a membership database of  members with the promise of introducing you to single Muslim men and women across the world.</p>
+                
+                </div>
+            
+            </div>
+            
+            <div class="container-right float-l">
+            
+            	<div class="head float-l"><h3>5 Reasons to Join M4P</h3></div>
+                
+                <div class="content float-l">
+                
+                	<ul>
+                    <li>Unique and professional  
+approach.</li>
+<li>100% Safe and secure.</li>
+<li>Great online features and use of intelligent match making tools.</li>
+<li>Verified contacts.</li>
+<li>100% guarantee to find a match.</li>
+</ul>
+<a href="register.html">
+<img src="images/join-free.png" width="228" height="78" alt="Musilm-Praposel" class="butt-margin" /></a>
+                
+                
+                </div>
+                <div class="head float-l"><h3>Charities</h3></div>
+                
+                <div class="content float-l">We at M4P work with charities across the world to raise money and awareness for those who are less fortunate. This website can help other people to around the world to find peace and in return bless our website user to find their eternal happiness through marriage. Insh'Allah.</div>
+                
+                <div class="halal float-r"><div class="float-l" style="width:160px;">
+                <p>Click to read why we are the Halal Choice.</p>
+                
+                </div>
+                
+                <div class="float-l"><img src="images/halal.jpg" width="113" height="114" alt="Musilm Praposel"/></div>
+                </div>
+            
+            </div>
+            <div style="clear:both;"></div>
+        
+        </div>
+        
+        </div>
+
+<?php include('include/footer.php'); ?>
+        
+        
+    
+    </div>
+
+</div>
+</body>
+</html>
